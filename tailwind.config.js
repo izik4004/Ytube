59a7eb90ce05d61d078aa7931ee0dd481c9f4019
izik4004 <izik4004@gmail.com>
2021-03-07 16:59:20 +0100

    added icons

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    
+    extend: {
+      colors: {
+        my: '#181818',
+        urs: '#202020',
+        
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
